c6528f83c07201882e8ca5d616c2c298ef4693c3
Priya <priya.rajput_cs18@gla.ac.in>
2020-11-08 20:31:40 +0530

    added changes in js file

--- to-doList/todo.js
+++ to-doList/todo.js
@@ -12,6 +12,7 @@ todoList.onclick = checkdelete;
 //function
 function create(e){
     e.preventDefault();
+    if (todoInput.value.length>0){
     var newDiv = document.createElement("div");
     newDiv.classList.add("todo");
 
@@ -21,27 +22,31 @@ function create(e){
     newDiv.appendChild(newLi);
 
     var checkbtn = document.createElement("button");
-    checkbtn.classList.add("check-btn")
+    checkbtn.classList.add("check-btn");
     checkbtn.innerHTML = '<i class = "fa fa-check"></i>';
     newDiv.appendChild(checkbtn);
 
     var deletebtn = document.createElement("button");
-    deletebtn.classList.add("delete-btn")
+    deletebtn.classList.add("delete-btn");
     deletebtn.innerHTML = '<i class = "fa fa-trash"></i>';
     newDiv.appendChild(deletebtn);
 
     todoList.appendChild(newDiv);
+    todoInput.value = '';
+    } else {
+        alert("You need to input a task!!");
+    }
 
 }
 
 function checkdelete(e){
     var item = e.target;
     if(item.classList[0]==="delete-btn"){
-        var parent  = item.parentNode
+        var parent  = item.parentNode;
         parent.remove();
     }
     if(item.classList[0]==="check-btn"){
-        var parent  = item.parentNode
+        var parent  = item.parentNode;
         parent.classList.toggle("completed");
     }
 }
